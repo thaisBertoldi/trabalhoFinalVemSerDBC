351f94fc603516530318b9506bc1eae5c23f417d
Thais B <thais_brt@hotmail.com>
2022-04-20 17:08:13 -0300

    create theme global

---
+++ src/allTheme.tsx
@@ -0,0 +1,23 @@
+export const Theme = {
+  color: {
+    primary: "#158A8A",
+    second: "#099A9A",
+    black: "#313131",
+    grayDark: "#5F5F5F",
+    grayLight: "#878787",
+    white: "#FFFFFF",
+  },
+  padding: "15px",
+  margin: "15px",
+  fontSize: "14px",
+  container: {
+    width: "1152px",
+    height: "820px",
+  },
+  button: {
+    width: "322px",
+    heigth: "42px",
+    borderRadius: "4px",
+  },
+  fontFamily: "'Poppins', sans-serif",
+};
