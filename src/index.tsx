82532c577e484183cd191a5ff0e9fd8c1bfd4532
Guilherme Denck <guidenck0303@gmail.com>
2022-04-20 17:08:05 -0300

    iniciando config do redux com erro de state undefined

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,17 @@ import './index.css';
 import Router from './routes';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import store from './store'
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Router />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </React.StrictMode>
 );
 
