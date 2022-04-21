266a0ca76c867d7243044d4d086b09f0f0305a85
Thais B <thais_brt@hotmail.com>
2022-04-21 11:39:19 -0300

    create branch test, try private routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,26 +1,50 @@
-import { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { Header, Footer } from "./components";
+import { Login, Register, Home } from "./pages";
+import api from "./service/api";
 
-import { Header, Footer } from './components';
-import { Login, Register, Home } from './pages';
+const Routers = ({ auth, dispatch }: any) => {
+  const [isLogged, setIsLogged] = useState(false);
 
-const Routers = () => {
+  const hasToken = () => {
+    if (auth.isLogged) {
+      const dataLocalToken: any = localStorage.getItem("token");
+      const convertDataToken = JSON.parse(dataLocalToken);
+      console.log(convertDataToken.token);
+      if (convertDataToken.token) {
+        api.defaults.headers.common["Authorization"] = convertDataToken.token;
+        console.log("We have a token");
+        setIsLogged(true);
+      }
+    }
+  };
 
-  useEffect( () => {
-    
-  },[] )
+  useEffect(() => {
+    hasToken();
+  }, []);
 
   return (
     <BrowserRouter>
       <Header />
-        <Routes>
-          <Route path='/' element={ <Home /> } />
-          <Route path='/login' element={ <Login /> }/>
-          <Route path='/register' element={ <Register /> }/>
-        </Routes>
+      <Routes>
+        {isLogged ? (
+          <Route path="/" element={<Home />} />
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </>
+        )}
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
-}
+};
 
-export default Routers;
+const mapStateToProps = (state: any) => ({
+  auth: state.authReducer,
+});
+
+export default connect(mapStateToProps)(Routers);
