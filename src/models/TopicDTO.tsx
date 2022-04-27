0a8290a456a157eb1180e046ab4d301da4d06956
Guilherme Denck <guidenck0303@gmail.com>
2022-04-27 14:31:29 -0300

    ajustando home tirando any e adicionando TopicDTO

---
+++ src/models/TopicDTO.tsx
@@ -0,0 +1,7 @@
+export interface TopicDTO {
+  title: string
+  date: string
+  status: string
+  topicId: number
+  totalValue: number
+}
--- src/pages/Home/Home.screen.tsx
+++ src/pages/Home/Home.screen.tsx
@@ -20,14 +20,15 @@ import { getTopics } from "../../store/action/topicActions";
 import { ModalDTO } from "../../models/ModalsDTO";
 import { IconSearch } from "../../global.style";
 import api from "../../service/api";
+import { TopicDTO } from "../../models/TopicDTO";
 
 const Home = ({ user, dispatch }: isLoggedDTO & DispatchProp) => {
   const navigate = useNavigate();
   const hasUser: string | any = localStorage.getItem("token");
   const User = JSON.parse(hasUser);
 
-  const [listTopics, setListTopics] = useState<any>([]);
-  const [listSearched, setListSearched] = useState<any>([]);
+  const [listTopics, setListTopics] = useState<Array<TopicDTO>>([]);
+  const [listSearched, setListSearched] = useState<Array<TopicDTO>>([]);
   const [allPages, setAllPages] = useState<number>(0);
   const [page, setPage] = useState<number>(0);
 
@@ -88,8 +89,8 @@ const Home = ({ user, dispatch }: isLoggedDTO & DispatchProp) => {
 
       <ContainerAllInfo>
         {
-          listTopics?.content?.length > 0 ? (
-            listTopics?.content?.map((item: any) => (
+          listTopics.length > 0 ? (
+            listTopics?.map((item: TopicDTO) => (
               <CardTopicHome
                 item={item}
                 user={user}
@@ -124,7 +125,7 @@ const Home = ({ user, dispatch }: isLoggedDTO & DispatchProp) => {
       </ContainerAllInfo>
       <CenterCustom>
         {
-          listTopics?.content?.length > 0 && (
+          listTopics?.length > 0 && (
             <Pagination page={page} onPageChange={(index: number) => setPage(index)} allPages={allPages}/>
           )
         }
